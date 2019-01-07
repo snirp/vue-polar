3a62f212fcbed4cc09be04e6fa8bd2dfa1b0a165
Roy Prins <prinsroy@gmail.com>
2019-01-07 16:02:45 +0100

    Fix v-for bug and add examples

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,90 @@
 <template>
   <div id="app">
-    <div class="test">
-      Test with referenced prop object + nested styles object
-      <polar v-bind="propObj" v-for="n in 12" :key="n" :item="n">{{n}}</polar>
+
+    <div class="example">
+      <polar :item="0" :segments="5">🚀</polar>
+      <polar :item="1" :segments="5">🚌</polar>
+      <polar :item="2" :segments="5">🚲</polar>
+      <polar :item="3" :segments="5">🚕</polar>
+      <polar :item="4" :segments="5">🚤</polar>
+    </div>    
+
+    <div class="example">
+      <polar 
+        v-for="n in 8" 
+        :key="n" 
+        :item="n" 
+        :segments="5"
+        :zeroAngle="0"
+        :setStraight="false"
+        :extraRotation="60"
+        :width="'100px'"
+        :height="'100px'"
+        :offset="'120%'"
+      >
+        <img src="https://cataas.com/cat" width="100%">
+      </polar>
     </div>
-    <hr/>
-    <div class="test">
-      Test with inline prop object + nested styles object
-      <polar v-bind="{segments:12,customStyles:{borderRadius:'50%', backgroundColor:'pink'}}" v-for="n in 12" :key="n" :item="n">{{n}}</polar>
+
+    <div class="example">
+      <!-- The hour numbers -->
+      <polar v-bind="numbersPropsObj" v-for="n in 12" :key="n" :item="n">
+        <div class='clock-number'>
+          {{n}}
+        </div>
+      </polar>
+      <!-- The hour markers -->
+      <polar v-bind="hoursPropsObj" v-for="n in 12" :key="n" :item="n" />
+      <!-- The minute markers -->
+      <polar v-bind="minutesPropsObj" v-for="n in 60" :key="n" :item="n" />
+      <!-- The hour hand -->
+      <polar 
+        :item="9"
+        :segments="60" 
+        :width="'100px'"
+        :height="'10px'"
+        :offset="'40px'"
+        :zeroAngle="270"
+        :setStraight="false"
+        :customStyles="{
+          backgroundColor: 'black',
+          borderRadius: '999px'
+        }"
+      />
+      <!-- The minute hand -->
+      <polar 
+        :item="51"
+        :segments="60" 
+        :width="'120px'"
+        :height="'6px'"
+        :offset="'50px'"
+        :zeroAngle="270"
+        :setStraight="false"
+        :zIndex="99"
+        :customStyles="{
+          backgroundColor: 'black',
+          borderRadius: '999px'
+        }"
+      />
     </div>
-    <hr/>
-    <div class="test">
-      Test with referenced prop object, no styles object
-      <polar v-bind="propObjNoNested" v-for="n in 12" :key="n" :item="n">{{n}}</polar>
+
+    <div class="example">
+      <polar
+        v-for="n in 60" 
+        :key="n" 
+        :item="n"
+        :segments="-30.3" 
+        :offset="n*3+'px'"
+        :zIndex="n"
+        :width="(2+n/2)+'px'"
+        :height="(2+n/2)+'px'"
+        :customStyles="{
+          borderRadius: '50%',
+          backgroundColor: '#'+('000000'+Math.floor(0xFFFFFF*(n/60)).toString(16)).slice(-6),
+        }"
+      />
     </div>
+
   </div>
 </template>
 
@@ -21,17 +92,34 @@
 import Polar from './components/Polar.vue'
 
 export default {
-  data(){
-    return {
-      propObj: {
-        segments: 12,
-        customStyles: {
-          borderRadius: '50%',
-          backgroundColor: 'pink'
-        }
-      },
-      propObjNoNested: {
-        segments: 12,
+  created(){
+    this.numbersPropsObj = {
+      segments: 12,
+      zeroAngle: 270,
+      offset: '150px',
+      width: '40px',
+      height: '40px'
+    };
+    this.hoursPropsObj = {
+      segments: 12,
+      width: '20px',
+      height: '5px',
+      offset: '110px',
+      setStraight: false,
+      zIndex: 2,
+      customStyles: {
+        backgroundColor: 'red'
+      }
+    };
+    this.minutesPropsObj = {
+      segments: 60,
+      width: '10px',
+      height: '3px',
+      offset: '115px',
+      setStraight: false,
+      zIndex: 1,
+      customStyles: {
+        backgroundColor: 'grey'
       }
     }
   },
@@ -43,10 +131,21 @@ export default {
 </script>
 
 <style>
-.test{
+.example{
   position: relative;
   width: 400px;
-  height: 200px;
+  height: 400px;
+  font-size: 2.5rem;
 }
-
+.clock-number{
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  color: white;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+}
 </style>
--- src/components/Polar.vue
+++ src/components/Polar.vue
@@ -1,5 +1,5 @@
 <template functional>
-  <div :style="Object.assign(props.customStyles, {
+  <div :style="Object.assign({}, props.customStyles, {
     width: props.width,
     height: props.height,
     margin: (-1*parseFloat(props.height)/2) + (props.height.match(/([a-z]*|%)$/)[0]) + ' ' 
@@ -7,7 +7,7 @@
     transform: `
       rotate(${props.item/props.segments*360+props.zeroAngle}deg) 
       translate(${props.offset}) 
-      rotate(-${(props.setStraight && props.item/props.segments*360) + props.zeroAngle + props.extraRotation}deg)`,
+      rotate(${(props.setStraight && -props.item/props.segments*360 - props.zeroAngle) + props.extraRotation}deg)`,
     zIndex: props.zIndex,
   })">
     <slot />
@@ -17,42 +17,56 @@
 <script>
 export default {
   props: {
+    // Custom style object, to be merged with calculated styles.
+    // The following properties will be overwritten by calculated properties:
+    //   width, height, margin, transform, zIndex, position, top, left
+    // Other CSS properties can be used.
     customStyles: {
       type: Object,
       default: () => ({})
     },
+    // Translation away from the center point
     offset: {
       type: [Number, String],
       default: '100%'
     },
+    // Height of polar element (relative to its own axis)
     height: {
       type: [Number, String],
       default: '50px'
     },
+    // Width of polar element (relative to its own axis)
     width: {
       type: [Number, String],
       default: '50px'
     },
+    // Starting angle relative to default zero angle (right / east)
     zeroAngle: {
-      // Needs to be positive
       type: Number,
       default: 0
     },
+    // Reset polar element's own rotation
     setStraight: {
       type: Boolean,
       default: true
     },
+    // Additional rotation in degrees
     extraRotation: {
       type: Number,
       default: 0
     },
-    item: {
-      type: Number,
-      default: 2
-    },
+    // Number of equal subdivisions of 360 degrees. Can be a decimal number.
+    // A positive number: clockwise rotation
+    // A negative number: counter-clockwise rotation 
     segments: {
       type: Number,
-      default: 5
+      default: 12
+    },
+    // Cardinal number that determines relative rotation
+    // Relative angle is calculated by: item/segments * 360
+    item: {
+      type: Number,
+      default: 0
     },
     zIndex: {
       type: Number,
